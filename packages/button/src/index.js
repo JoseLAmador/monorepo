b9fc29250a163a0fe7634c079531262db38710a6
JoseLAmador <josel.amador.s@gmail.com>
2019-07-18 12:15:26 -0500

    fix: custom button

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-    background: red;
+    background: blue;
     color: #fff;
     border-radius: 4px;
     cursor: pointer;
--- packages/button/src/index.story.js
+++ packages/button/src/index.story.js
@@ -4,4 +4,4 @@ import {storiesOf} from '@storybook/react';
 
 import Button from '.';
 
-storiesOf('Button', module).add('default', () => <Button>{'Button'}</Button>);
+storiesOf('Button', module).add('default', () => <Button>{'Login with Facebook'}</Button>);
