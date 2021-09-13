f50ecd5bae5a071f740a5336b6a546a6d78d3a2a
JoseLAmador <josel.amador.s@gmail.com>
2021-09-13 18:06:11 -0500

    fix: move packages to peerDependencies

--- packages/docs-site/next.config.js
+++ packages/docs-site/next.config.js
@@ -2,8 +2,5 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-  transpileModules: [
-    /^\@asjosel\/.*/
-  ]
 });
 module.exports = withNextra()
