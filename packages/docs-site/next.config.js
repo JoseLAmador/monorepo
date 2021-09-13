132d1a036e92dad38dc96375c59890dc20bc41d6
JoseLAmador <josel.amador.s@gmail.com>
2021-09-13 17:55:24 -0500

    fix: minor fix

--- packages/docs-site/next.config.js
+++ packages/docs-site/next.config.js
@@ -2,9 +2,8 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-  webpack: (config) => {
-    config.resolve.symlinks = false
-    return config
-  }
+  transpileModules: [
+    /^\@asjosel\/.*/
+  ]
 });
 module.exports = withNextra()
