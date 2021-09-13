90a25894e862b8c55d6c55c8c19a20212f4ba8fd
JoseLAmador <josel.amador.s@gmail.com>
2021-09-13 17:44:43 -0500

    chore: modify webpack config

--- packages/docs-site/next.config.js
+++ packages/docs-site/next.config.js
@@ -2,5 +2,9 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-})
+  webpack: (config) => {
+    config.resolve.symlinks = false
+    return config
+  }
+});
 module.exports = withNextra()
