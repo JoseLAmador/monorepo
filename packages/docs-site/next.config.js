af21c8cb48dea697a23b9834e07a586716f81d1c
JoseLAmador <josel.amador.s@gmail.com>
2021-09-13 18:38:54 -0500

    fix: transpile module

--- packages/docs-site/next.config.js
+++ packages/docs-site/next.config.js
@@ -2,9 +2,8 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-  webpack: (config) => {
-    config.resolve.symlinks = true
-    return config
-  }
 });
-module.exports = withNextra()
+
+const withTranspile = require('next-transpile-modules')(['@asjosel/core', '@asjosel/tabs'])
+
+module.exports = withTranspile(withNextra())
