b7a14466b41586621a5c75f9d85adc460f2c6da5
JoseLAmador <josel.amador.s@gmail.com>
2021-09-14 10:53:54 -0500

    fix: cleaned

--- packages/docs-site/next.config.js
+++ packages/docs-site/next.config.js
@@ -1,9 +1,29 @@
+const path = require("path");
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
 });
 
-const withTm = require('next-transpile-modules')(['@asjosel/core', '@asjosel/tabs'])
+const nextConfig = {
+  webpack: function (config, { defaultLoaders }) {
+    const resolvedBaseUrl = path.resolve(config.context, "../../");
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(tsx|ts|js|mjs|jsx)$/,
+        include: [resolvedBaseUrl],
+        use: defaultLoaders.babel,
+        exclude: (excludePath) => {
+          return /node_modules/.test(excludePath);
+        },
+      },
+    ];
+    return config;
+  },
+};
 
-module.exports = withNextra(withTm())
+module.exports = withNextra({
+  ...nextConfig
+});
